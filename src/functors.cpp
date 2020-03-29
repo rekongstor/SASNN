ca5ba10212ad6b9b93078ebcec75294aa3d4cf3d
Paul <rekongstor@mail.ru>
2020-03-29 06:39:30 +0300

    Fixed sub & mul functors

--- src/functors.cpp
+++ src/functors.cpp
@@ -6,11 +6,11 @@ f32 sum(f32 left, f32 right) {
 }
 
 f32 sub(f32 left, f32 right) {
-    return left * right;
+    return left - right;
 }
 
 f32 mul(f32 left, f32 right) {
-    return left - right;
+    return left * right;
 }
 
 f32 div(f32 left, f32 right) {
--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@ int main() {
     Matrix2D matrix2Darg(4, 3);
     matrix2D.CellOperator(matrix2Darg, expf);
     matrix2D.CellOperator(matrix2Darg, expf);
-    matrix2D.CellOperator(matrix2Darg, expf);
+    Matrix2D s(1,3);
+    s.MergeRowOperator(matrix2D,mul);
 
-
-    for (size_t i = 0; i < matrix2D.getRows(); ++i) {
-        for (size_t j = 0; j < matrix2D.getCols(); ++j)
-            std::cout << matrix2D(i, j) << " ";
+    for (size_t i = 0; i < s.getRows(); ++i) {
+        for (size_t j = 0; j < s.getCols(); ++j)
+            std::cout << s(i, j) << " ";
         std::cout << std::endl;
     }
     return 0;
