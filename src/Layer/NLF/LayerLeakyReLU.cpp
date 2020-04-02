4dd0303062d9c614a3d1ed3625280ff2a421cec1
Paul <rekongstor@mail.ru>
2020-04-03 02:30:40 +0300

    Removed unnecessary std::move from Dataset initialization. LeakyReLU Matrix2D param replaced with f32

--- src/Layer/NLF/LayerLeakyReLU.cpp
+++ src/Layer/NLF/LayerLeakyReLU.cpp
@@ -1,7 +1,7 @@
 #include "../../../include/Layer/NLF/LayerLeakyReLU.h"
 
 void LayerLeakyReLU::followProp() {
-    data.CellOperator(left.getData(), right.getData(), [](const f32 l, const f32 r) -> f32 {
+    data.CellOperator(left.getData(), leak, [](const f32 l, const f32 r) -> f32 {
         return l <= 0.f ? l * r : l;
     });
 }
@@ -9,15 +9,15 @@ void LayerLeakyReLU::followProp() {
 void LayerLeakyReLU::backProp() {
     if (left.getGrad() != nullptr) {
         Matrix2D &g = *left.getGrad();
-        g.CellOperator(left.getData(), right.getData(), [](const f32 l, const f32 r) -> f32 {
+        g.CellOperator(left.getData(), leak, [](const f32 l, const f32 r) -> f32 {
             return l <= 0.f ? r : 1.f;
         }, &grad);
     }
     // right is a hyper-parameter and will not be changer with gradient descent
 }
 
-LayerLeakyReLU::LayerLeakyReLU(Layer &left, Layer &right) : LayerDynamic(left.getData().getRows(), left.getData().getCols()),
+LayerLeakyReLU::LayerLeakyReLU(Layer &left, f32 leak) : LayerDynamic(left.getData().getRows(), left.getData().getCols()),
                                                             left(left),
-                                                            right(right) {
+                                                            leak(leak) {
 
 }
