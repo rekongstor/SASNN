42251d6e0ed0168f1298bb3e8fee256a16e560e7
Paul <rekongstor@mail.ru>
2020-04-04 02:39:41 +0300

    Fixed BatchNormalization backProp. Added default leak for LayerLeakReLU = 0.01. Changed L2Reg

--- src/Layer/Functional/LayerL2Reg.cpp
+++ src/Layer/Functional/LayerL2Reg.cpp
@@ -1,26 +1,29 @@
 #include "../../../include/Layer/Functional/LayerL2Reg.h"
 
 void LayerL2Reg::followProp() {
+    multiplier.setCell(0, 0, right.getData()(0, 0) / static_cast<f32>(rowOriented ? left.getData().getRows() : left.getData().getCols()));
     data.MergeCellsOperator(left.getData(), [](const f32 l, const f32 r) -> f32 {
         return l + r * r;
-    }, nullptr, [](const f32 l) -> f32 {
-        return l * l;
-    });
-    data.CellOperator(data,right.getData(), [](const f32 l, const f32 r) -> f32{
+        }, nullptr, [](const f32 l) -> f32 {
+            return l * l;
+        });
+    data.CellOperator(data, multiplier, [](const f32 l, const f32 r) -> f32 {
         return l * r;
-    });
+        });
 }
 
 void LayerL2Reg::backProp() {
     if (left.getGrad() != nullptr) {
-        Matrix2D &g = *left.getGrad();
-        g.CellOperator(left.getData(),right.getData(), [](const f32 l, const f32 r) -> f32 {
+        Matrix2D& g = *left.getGrad();
+        g.CellOperator(left.getData(), multiplier, [](const f32 l, const f32 r) -> f32 {
             return 2.f * l * r;
-        }, &grad);
+            }, &grad);
     }
 }
 
-LayerL2Reg::LayerL2Reg(Layer &left, Layer &param) : LayerDynamic(1, 1),
-                                                    left(left),
-                                                    right(param) {}
+LayerL2Reg::LayerL2Reg(Layer& left, Layer& param, bool rowOriented) : LayerDynamic(1, 1),
+left(left),
+right(param),
+multiplier(1, 1),
+rowOriented(rowOriented) {}
 
